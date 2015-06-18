414d78e7cc68708f12147e6454f6a7bd499824d5
Scott <scottckizer@yahoo.com>
2015-06-18 10:56:34 -0700

    Changes to Register & Log In Pages.

--- bike_race/resources/views/auth/login.blade.php
+++ bike_race/resources/views/auth/login.blade.php
@@ -1,70 +1,75 @@
-@extends('app')
+@extends('layout')
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Log In</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+@section('main')
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+@if (count($errors) > 0)
+	<div class="alert alert-danger">
+		<strong>Whoops!</strong> There were some problems with your input.<br><br>
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
+@endif
 
-						{{-- <div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div> --}}
+<div class="hero">
+    <div class="large_photo_border">
+        <!-- <div class="large_photo"></div> -->
+        <img src="/images/rider_cropped.jpg">
+    </div>
+</div>
 
-						<div>
-			                <h4>E-mail:</h4>
-            			    <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
-            			</div>
+<form class="bars log_in" role="form" method="POST" action="{{ url('/auth/login') }}">
+	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						{{-- <div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div> --}}
+	<section>
 
-						<div>
-			                <h4>Password:</h4>
-            			    <input type="password" name="password" placeholder="Password">
-            			</div> 
+		{{-- <div class="form-group">
+			<label class="col-md-4 control-label">E-Mail Address</label>
+			<div class="col-md-6">
+				<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+			</div>
+		</div> --}}
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
+		<div>
+	        <h4>E-mail:</h4>
+		    <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
+		</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Log In</button>
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
+		{{-- <div class="form-group">
+			<label class="col-md-4 control-label">Password</label>
+			<div class="col-md-6">
+				<input type="password" class="form-control" name="password">
+			</div>
+		</div> --}}
+
+		<div>
+	        <h4>Password:</h4>
+		    <input type="password" name="password" placeholder="Password">
+		</div> 
+
+		<button type="submit">
+			Log In
+		</button>
+
+		{{-- <div class="form-group">
+			<div class="col-md-6 col-md-offset-4">
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" name="remember"> Remember Me
+					</label>
 				</div>
 			</div>
-		</div>
-	</div>
-</div>
+		</div> --}}
+
+		{{-- <div class="form-group">
+			<div class="col-md-6 col-md-offset-4">
+				<button type="submit" class="btn btn-primary">Log In</button>
+				<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+			</div>
+		</div> --}}
+	</section>
+</form>
+				
 @endsection
--- bike_race/resources/views/auth/register.blade.php
+++ bike_race/resources/views/auth/register.blade.php
@@ -1,114 +1,111 @@
-@extends('app')
+@extends('layout')
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Sign Up</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+@section('main')
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="input_photo">
-                			<a href="">
-                    			<img src="">
-                				<p>Add Photo</p>
-                			</a>
-            			</div>
-
-						{{-- <div class="form-group">
-							<label class="col-md-4 control-label">First Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
-							</div>
-						</div> --}}
-
-						<div>
-                			<h4>First Name:</h4>
-                			<input type="text" name="first_name" placeholder="First Name" value="{{ old('first_name') }}">
-            			</div>
-
-						{{-- <div class="form-group">
-							<label class="col-md-4 control-label">Last Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
-							</div>
-						</div> --}}
-
-						<div>
-			                <h4>Last Name:</h4>
-            			    <input type="text" name="last_name" placeholder="Last Name" value="{{ old('last_name') }}">
-            			</div>
-
-						{{-- <div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div> --}}
-
-						<div>
-                			<h4>E-mail:</h4>
-                			<input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
-            			</div>
-
-						{{-- <div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div> --}}
-
-						<div>
-                			<h4>Password:</h4>
-                			<input type="password" name="password" placeholder="Password">
-            			</div>
-
-						{{-- <div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div> --}}
-
-						<div>
-                			<h4>Confirm Password:</h4>
-                			<input type="password" name="password_confirmation" placeholder="Confirm Password">
-            			</div>
-
-            			<div>
-			                <h4>Hometown:</h4>
-            			    <input type="text" name="hometown" placeholder="Hometown">
-            			</div>
-
-            			<div>
-                			<h4>Birthday:</h4>
-                			<input type="date" name="birthday">
-            			</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Sign Up
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
+@if (count($errors) > 0)
+	<div class="alert alert-danger">
+		<strong>Whoops!</strong> There were some problems with your input.<br><br>
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
 	</div>
+@endif
+
+<div class="hero">
+    <div class="large_photo_border">
+        <!-- <div class="large_photo"></div> -->
+        <img src="/images/rider_cropped.jpg">
+    </div>
 </div>
+
+<form class="bars register" role="form" method="POST" action="{{ url('/auth/register') }}">
+	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+	<section>
+		<div class="input_photo">
+			<a href="">
+				<img src="">
+				<p>Add Photo</p>
+			</a>
+		</div>
+
+		{{-- <div class="form-group">
+			<label class="col-md-4 control-label">First Name</label>
+			<div class="col-md-6">
+				<input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
+			</div>
+		</div> --}}
+
+		<div>
+			<h4>First Name:</h4>
+			<input type="text" name="first_name" placeholder="First Name" value="{{ old('first_name') }}">
+		</div>
+
+		{{-- <div class="form-group">
+			<label class="col-md-4 control-label">Last Name</label>
+			<div class="col-md-6">
+				<input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
+			</div>
+		</div> --}}
+
+		<div>
+	        <h4>Last Name:</h4>
+		    <input type="text" name="last_name" placeholder="Last Name" value="{{ old('last_name') }}">
+		</div>
+
+		{{-- <div class="form-group">
+			<label class="col-md-4 control-label">E-Mail Address</label>
+			<div class="col-md-6">
+				<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+			</div>
+		</div> --}}
+
+		<div>
+			<h4>E-mail:</h4>
+			<input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
+		</div>
+
+		{{-- <div class="form-group">
+			<label class="col-md-4 control-label">Password</label>
+			<div class="col-md-6">
+				<input type="password" class="form-control" name="password">
+			</div>
+		</div> --}}
+
+		<div>
+			<h4>Password:</h4>
+			<input type="password" name="password" placeholder="Password">
+		</div>
+
+		{{-- <div class="form-group">
+			<label class="col-md-4 control-label">Confirm Password</label>
+			<div class="col-md-6">
+				<input type="password" class="form-control" name="password_confirmation">
+			</div>
+		</div> --}}
+
+		<div>
+			<h4>Confirm Password:</h4>
+			<input type="password" name="password_confirmation" placeholder="Confirm Password">
+		</div>
+
+		<div>
+	        <h4>Hometown:</h4>
+		    <input type="text" name="hometown" placeholder="Hometown">
+		</div>
+
+		<div>
+			<h4>Birthday:</h4>
+			<input type="date" name="birthday">
+		</div>
+
+		<button type="submit">
+			Sign Up
+		</button>
+	</section>
+
+</form>
+
 @endsection
